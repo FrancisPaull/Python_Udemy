ef5a11d91ca118f9859cda18e422e475763f4fcf
Francispaull <francis.rocha_@hotmail.com>
2023-01-30 07:36:31 -0300

    30-01-22

---
+++ .vscode/exercicios/aula072.py
@@ -0,0 +1,14 @@
+# Exercícios com funções
+
+# Crie uma função que multiplica todos os argumentos
+# não nomeados recebidos
+# Retorne o total para uma variável e mostre o valor
+# da variável.
+
+def multiplica(*args):
+    total = 1
+    for numero in args:
+        total *= numero
+    return total
+
+print(multiplica(1,2,3,4,5))
---
+++ .vscode/exercicios/aula075.py
@@ -0,0 +1,5 @@
+
+
+def funcao(n1, n2):
+    return funcao
+
